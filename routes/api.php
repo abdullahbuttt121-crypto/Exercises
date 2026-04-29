a0993442b2cf57bcca8fb9ca1ca365ff1813598e
Abdullah7772 <abddullah.butt123@gmail.com>
2026-04-29 12:40:12 +0500

    exercise-3

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Exercise_1;
 use App\Http\Controllers\Exercise_2;
+use App\Http\Controllers\Exercise_3;
 
 Route::get('/test', function () {
     return response()->json([
@@ -13,3 +14,4 @@ Route::get('/test', function () {
 
 Route::post("/exercise-1-artwork-version" , [Exercise_1::class, 'ArtworkVersion'])->name('exercise-1-artwork-version');
 Route::post("/exercise-2-tier-pricing" , [Exercise_2::class, 'PriceSelector'])->name('exercise-2-tier-pricing');
+Route::post("/exercise-3-cart-validator" , [Exercise_3::class, 'cartValidator'])->name('exercise-2-tier-pricing');
