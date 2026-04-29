613abcd1ed6bdb01651b50f9ac407d0307c5bc9d
Abdullah7772 <abddullah.butt123@gmail.com>
2026-04-29 19:47:26 +0500

    command

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,6 @@ Route::get('/test', function () {
     ]);
 });
 
-Route::post("/exercise-1-artwork-version" , [Exercise_1::class, 'ArtworkVersion'])->name('exercise-1-artwork-version');
-Route::post("/exercise-2-tier-pricing" , [Exercise_2::class, 'PriceSelector'])->name('exercise-2-tier-pricing');
-Route::post("/exercise-3-cart-validator" , [Exercise_3::class, 'cartValidator'])->name('exercise-2-tier-pricing');
+Route::post("/exercise-1-artwork-version" , [Exercise_1::class, 'ArtworkVersion']);
+Route::post("/exercise-2-tier-pricing" , [Exercise_2::class, 'PriceSelector']);
+Route::post("/exercise-3-cart-validator" , [Exercise_3::class, 'cartValidator']);
