62a964177321ebdec0efeb61ce8ddaa34e423a20
Abdullah7772 <abddullah.butt123@gmail.com>
2026-05-06 16:17:41 +0500

    Update 6 and 7

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use App\Http\Controllers\Exercise_2;
 use App\Http\Controllers\Exercise_3;
 use App\Http\Controllers\Exercise_4;
 use App\Http\Controllers\Exercise_5;
+use App\Http\Controllers\Exercise_6;
+use App\Http\Controllers\Exercise_7;
 
 Route::get('/test', function () {
     return response()->json([
@@ -19,3 +21,5 @@ Route::post("/exercise-2-tier-pricing" , [Exercise_2::class, 'PriceSelector']);
 Route::post("/exercise-3-cart-validator" , [Exercise_3::class, 'cartValidator']);
 Route::post("/exercise-4-vendor-allocation" , [Exercise_4::class, 'vendorAllocation']);
 Route::post("/exercise-5-discount", [Exercise_5::class , "discountCalculator"]);
+Route::post("/exercise-6-approval-flow", [Exercise_6::class , "approvalFlow"]);
+Route::post("/exercise-7-inventory", [Exercise_7::class , "inventoryManagement"]);
