a79b3d9af01372d7483900bb5fcc7279e4edde14
Abdullah7772 <abddullah.butt123@gmail.com>
2026-05-11 17:51:14 +0500

    exercise-10

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Exercise_1;
+use App\Http\Controllers\Exercise_10;
 use App\Http\Controllers\Exercise_2;
 use App\Http\Controllers\Exercise_3;
 use App\Http\Controllers\Exercise_4;
@@ -27,3 +28,4 @@ Route::post("/exercise-6-approval-flow", [Exercise_6::class , "approvalFlow"]);
 Route::post("/exercise-7-inventory", [Exercise_7::class , "inventoryManagement"]);
 Route::post("/exercise-8-shipment", [Exercise_8::class , "shipmentFlow"]);
 Route::post("/exercise-9-webhook", [Exercise_9::class , "webhookHandler"]);
+Route::post("/exercise-10-quote-expiry", [Exercise_10::class , "dateChecker"]);
